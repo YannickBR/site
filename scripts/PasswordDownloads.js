3b2a4f2227da4d242d58a9014c9bfd990940f507
Yannick <yannickbruintjes@hotmail.com>
2018-04-11 20:31:05 +0200

    V4

---
+++ scripts/PasswordDownloads.js
@@ -0,0 +1,20 @@
+function passWord() {
+var testV = 1;
+var pass1 = prompt('Vul je wachtwoord in!','');
+while (testV < 3) {
+if (!pass1) 
+history.go(-1);
+if (pass1.toLowerCase() == "voorbeeld") {
+alert('Wachtwoord correct! Druk op OK om door te gaan!');
+location.replace('Website-downloadwwtrue.html');
+break;
+} 
+testV+=1;
+var pass1 = 
+prompt('Fout wachtworod!','');
+}
+if (pass1.toLowerCase()!="password" & testV ==3) 
+alert('Het lijkt erop dat je het wachtwoord niet weet! Je wordt nu terug gestuurd naar de hoofdpagina!')
+history.go(-1);
+return " ";
+} 
