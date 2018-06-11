4fe88dd08b69d06b0b15d93bb32db47cdf2090e8
Yannick <yannickbruintjes@hotmail.com>
2018-06-11 14:12:27 +0200

    VE

--- scripts/PasswordDownloads.js
+++ scripts/PasswordDownloads.js
@@ -6,12 +6,12 @@ if (!pass1)
 history.go(-1);
 if (pass1.toLowerCase() == "erwten") {
 alert('Wachtwoord correct! Druk op OK om door te gaan!');
-location.replace('downoadswwtrue.html');
+location.Replace(downoadswwtrue.html)
 break;
 } 
 testV+=1;
 var pass1 = 
-prompt('Fout wachtworod!','');
+prompt('Fout wachtworod!','');	
 }
 if (pass1.toLowerCase()!="password" & testV ==3) 
 alert('Het lijkt erop dat je het wachtwoord niet weet! Je wordt nu terug gestuurd naar de hoofdpagina!')
